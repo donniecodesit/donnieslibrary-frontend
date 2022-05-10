c3c2eb777d5c9354ee7d62388080cd4cf3bc4184
Donovan Laws <donovan.laws@gmail.com>
2022-05-09 22:48:00 -0700

    Code Cleanup

--- src/Games/Games.js
+++ src/Games/Games.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { listGames } from '../utils/api';
-import GamesList from './GamesList';
-import NewGame from './NewGame';
+import React, { useState, useEffect } from "react";
+import { listGames } from "../utils/api";
+import GamesList from "./GamesList";
+import NewGame from "./NewGame";
 
-export default function Games({admin}) {
-    const [games, setGames] = useState([]);
-    const [gamesError, setGamesError] = useState([]);
-    const [loaded, setLoaded] = useState(false);
+export default function Games({ admin }) {
+  const [games, setGames] = useState([]);
+  const [error, setError] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
-    useEffect(() => {
-        const abortController = new AbortController();
-        async function loadGames() {
-            setGamesError([]);
-            setLoaded(false);
-            listGames(abortController.signal)
-                .then((res) => {
-                    setGames(res.sort((gameA, gameB) => gameB.rating - gameA.rating))
-                    setLoaded(true);
-                })
-                .catch((err) => {
-                    setGames([]);
-                    setLoaded(true);
-                    setGamesError([err.message]);
-                })
-        }
+  useEffect(() => {
+    const abortController = new AbortController();
+    async function loadGames() {
+        setError([]);
+      setLoaded(false);
+      listGames(abortController.signal)
+        .then((res) => {
+          setGames(res.sort((gameA, gameB) => gameB.rating - gameA.rating));
+          setLoaded(true);
+        })
+        .catch((err) => {
+          setGames([]);
+          setLoaded(true);
+          setError([err.message]);
+        });
+    }
 
-        loadGames();
-        return () => abortController.abort();
-    }, [admin])
+    loadGames();
+    return () => abortController.abort();
+  }, [admin]);
 
-    return (
-        loaded && (
-            <div className="main">
-                {gamesError[0] && <div className="error">{gamesError}</div>}
-                {admin && <NewGame />}
-                <GamesList games={games} />
-            </div>
-        )
+  return (
+    loaded && (
+      <div className="main">
+        {error[0] && <div className="error">{error}</div>}
+        {admin && <NewGame />}
+        <GamesList games={games} />
+      </div>
     )
+  );
 }
