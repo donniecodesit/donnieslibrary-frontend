b1d059ceaeb81508f473a518de299b952e01d98f
Donovan Laws <donovan.laws@gmail.com>
2022-05-10 15:29:31 -0700

    Link fix

--- src/About/Header.js
+++ src/About/Header.js
@@ -32,7 +32,7 @@ export default function Header() {
                   event.preventDefault(); 
                   navigate('/');
               }}>
-                  Portfolio
+                  Game Library
               </a>
             </div>
           </div>
