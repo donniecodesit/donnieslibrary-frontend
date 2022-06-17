b20ef4d93af2a771bb1aee9249150712ba19abd0
Donovan Laws <donovan.laws@gmail.com>
2022-06-17 12:47:09 -0700

    Resolved Issues #4 and #5

--- src/Games/_Games.js
+++ src/Games/_Games.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { listGames } from "../utils/api";
 import GamesList from "./GamesList";
 import NewGame from "./NewGame";
-import './_Games.css'
+import "./_Games.css";
 
 export default function Games({ admin }) {
   const [games, setGames] = useState([]);
@@ -12,7 +12,7 @@ export default function Games({ admin }) {
   useEffect(() => {
     const abortController = new AbortController();
     async function loadGames() {
-        setError([]);
+      setError([]);
       setLoaded(false);
       listGames(abortController.signal)
         .then((res) => {
@@ -31,13 +31,23 @@ export default function Games({ admin }) {
   }, [admin]);
 
   return (
-    loaded && (
-      <div className="main" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <h1 className="title">Donnie's Game Library</h1>
-        {error[0] && <div className="error">{error}</div>}
-        {admin && <NewGame />}
-        <GamesList games={games} />
-      </div>
-    )
+    <div
+      className="main"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1 className="title">Donnie's Game Library</h1>
+      {loaded ? (
+        <>
+          {error[0] && <div className="error">{error}</div>}
+          {admin && <NewGame />}
+          <GamesList games={games} />
+        </>
+      ) : (
+        <>
+          <h2>Loading Game Library...</h2>
+          <img src="https://retchhh.files.wordpress.com/2015/03/loading1.gif" alt="...sdsdf" style={{ width: "30%" }} />
+        </>
+      )}
+    </div>
   );
 }
