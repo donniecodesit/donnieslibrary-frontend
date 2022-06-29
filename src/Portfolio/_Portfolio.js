9a62ed9267b910cc50d4861640e816651e228adc
Donovan Laws <91988909+donovanlaws@users.noreply.github.com>
2022-06-29 15:08:46 -0700

    Comment Feature Implemented, Beta

--- src/Portfolio/_Portfolio.js
+++ src/Portfolio/_Portfolio.js
@@ -5,21 +5,31 @@ import AboutMe from "./AboutMe";
 import PhotoPane from "./PhotoPane";
 import Skills from "./Skills";
 import Projects from "./Projects";
+import Comments from "./Comments";
 
 export default function Portfolio() {
+  const date = new Date();
   return (
-    <div className="Portfolio">
-      <main>
-        <div className="col-lg-4">
-          <PhotoPane />
-          <Skills />
-        </div>
-        <div className="col-lg-8">
-          <Introduction />
-          <AboutMe />
-          <Projects />
-        </div>
-      </main>
-    </div>
+    <>
+      <div className="Portfolio">
+        <main>
+          <div className="col-lg-4">
+            <PhotoPane />
+            <Skills />
+          </div>
+          <div className="col-lg-8">
+            <Introduction />
+            <AboutMe />
+            <Projects />
+            <Comments />
+          </div>
+        </main>
+      </div>
+      <footer>
+        <center>
+          <small>Copyright © {date.getFullYear()} Donovan Laws.</small>
+        </center>
+      </footer>
+    </>
   );
 }
